cdecbefca8b6afc3ae0f43a54979ddd0ddce2ada
yhk <ezenyhk@gmail.com>
2020-09-28 17:07:31 +0900

    Study polymorphism

---
+++ week04/java/polymorphism_field/Main.java
@@ -0,0 +1,33 @@
+package polymorphism_field;
+
+public class Main {
+    public static void main(String[] args) {
+        Car car = new Car();
+
+        for (int i = 1; i <= 5; i++) {
+            int problemLocation = car.run();
+
+            switch (problemLocation) {
+                case 1:
+                    System.out.println("앞 왼쪽 HankookTire로 교체");
+                    car.frontLeftTire = new HankookTire("앞왼쪽",15);
+                    break;
+                case 2:
+                    System.out.println("앞 오른쪽 KumhoTire로 교체");
+                    car.frontRightTire = new KumhoTire("앞오른쪽",13);
+                    break;
+                case 3:
+                    System.out.println("뒤 왼쪽 HankookTire로 교체");
+                    car.backLeftTire = new HankookTire("뒤왼쪽",14);
+                    break;
+                case 4:
+                    System.out.println("뒤 오른쪽 KumhoTire로 교체");
+                    car.backRightTire = new KumhoTire("뒤오른쪽",17);
+                    break;
+            }
+            System.out.println("--------------------------------");
+        }
+
+
+    }
+}
