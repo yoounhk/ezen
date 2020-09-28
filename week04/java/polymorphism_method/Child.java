a6ff5f1c14e91d2327aeb4881de4bca5367b480c
yhk <ezenyhk@gmail.com>
2020-09-28 17:07:48 +0900

    Study polymorphism

---
+++ week04/java/polymorphism_method/Child.java
@@ -0,0 +1,12 @@
+package polymorphism_method;
+
+public class Child extends Parent {
+    @Override
+    public void method2() {
+        System.out.println("Child-method2()");
+    }
+
+    public void method3() {
+        System.out.println("Child-method3()");
+    }
+}
