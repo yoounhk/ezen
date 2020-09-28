a30284d01ee248881ce5554b767752d4830b7056
yhk <ezenyhk@gmail.com>
2020-09-28 17:08:23 +0900

    Study protected keyword

---
+++ week04/java/protected_keyword/package2/C.java
@@ -0,0 +1,10 @@
+package protected_keyword.package2;
+public class C {
+    public void method() {
+        /*
+        A a= new A();    // (x)
+        a.field="value"; // (x)
+        a.method();      // (x)
+        */
+    }
+}
